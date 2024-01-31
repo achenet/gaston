77b1a362a559f54e1830c30942de072759741009
Ariel Chenet <apchenet@gmail.com>
2024-01-31 14:04:46 +0100

    Start name generation

---
+++ generate/name.go
@@ -0,0 +1,6 @@
+package generate
+
+func NewName(tm [][]float32) string {
+	out := ""
+	return out
+}
--- main.go
+++ main.go
@@ -3,14 +3,36 @@ package main
 import (
 	"log"
 	"os"
+
+	"github.com/achenet/gaston/generate"
 )
 
 const _DefaultInputFile = "input.txt"
 
 func main() {
+	text, err := getTextFromInputFile()
+	if err != nil {
+		log.Fatalf("could not get text from input file: %s", err.Error())
+	}
+	transitionMatrix := generate.Generate(text)
+}
+
+func getTextFromInputFile() (string, error) {
 	// read input file
 	args := os.Args
 	if len(args) < 2 {
 		log.Printf("no input file given as argument, using %s", _DefaultInputFile)
+		return read(_DefaultInputFile)
+	} else {
+		return read(os.Args[1])
 	}
+
+}
+
+func read(filename string) (string, error) {
+	b, err := os.ReadFile(filename)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
 }
